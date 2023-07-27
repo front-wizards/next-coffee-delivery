d3558a758c061cfd4ac781f369116753dff85345
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-07-27 20:22:35 -0300

    Navbar minor refactor

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Icon } from "../Icon/Icon";
-
 import { ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: string;
-  iconName?: "purpleCart" | "yellowCart" | "plus" | "minus" | "remove";
+  iconName?: "purpleCart" | "purpleMap" | "yellowCart" | "plus" | "minus" | "remove";
   styles?: string;
+  onClick?: () => void;
 }
 
-export function Button({ iconName, children, styles, ...rest }: ButtonProps) {
+export function Button({ iconName, children, styles, onClick, ...rest }: ButtonProps) {
   return (
-    <button type="button" {...rest} className={styles ? styles : 'p-2 border-2 bg-[#FFFF] rounded-lg'}>
+    <button
+      type="button"
+      {...rest}
+      className={`${styles} flex justify-between items-center gap-2`}
+      onClick={onClick}
+    >
       <Icon name={iconName} />
       {children}
     </button>
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,30 +1,39 @@
+'use client'
 import Image from "next/image"
 import { Button } from "../Button/Button"
+import Link from 'next/link'
 
 const Navbar = () => {
+
+
   return (
     <div className="flex justify-between items-center w-[1440px] py-8">
         <div>
-          <Image
-            src={"/Logo.png"}
-            alt="Coffee delivery Logo"
-            width={84.95}
-            height={40}
-          />
+          <Link href="/">
+            <Image
+              src={"/Logo.png"}
+              alt="Coffee delivery Logo"
+              width={84.95}
+              height={40}
+            />
+          </Link>
         </div>
         <div className="flex flex-row justify-around items-center gap-4">
             <div>
               <Button
+                iconName={'purpleMap'}
                 styles="purpleButton"
               >
                 Florianópolis,SC
               </Button>
             </div>
             <div>
+            <Link href="/checkout">
               <Button
                 iconName="yellowCart"
                 styles="yellowButton"
               />
+            </Link>
             </div>
         </div>
     </div>
