5432c7cad81b827db62ddacd6f3586cd21c3f723
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-07-28 00:02:25 -0300

    Intro

--- app/components/Intro/Intro.tsx
+++ app/components/Intro/Intro.tsx
@@ -1,6 +1,7 @@
+'use client'
 import Image from 'next/image'
 import  { baloo2, roboto } from '../../utils/fonts/fonts'
-
+import { ShoppingCart, Package, Timer, Coffee } from "@phosphor-icons/react";
 
 const Intro = () => {
   return (
@@ -17,6 +18,34 @@ const Intro = () => {
               Com o Coffee Delivery você recebe seu café onde estiver, a
               qualquer hora
             </p>
+          </div>
+          <div className="block pt-4 sm:flex sm:justify-between items-center sm:pt-16 sm:pb-6">
+            <div className="flex justify-between items-center gap-2">
+              <div className="rounded-full p-2 border bg-[#C47F17]">
+                <ShoppingCart color="#fff" weight="fill" size={15}/>
+              </div>
+              Compra simples e segura
+            </div>
+            <div className="flex justify-between items-center gap-2">
+              <div className="rounded-full p-2 border bg-[#574F4D]">
+                <Package color="#fff" weight="fill" size={15} />
+              </div>
+              Embalagem mantém o café intacto
+            </div>
+          </div>
+          <div className="block sm:flex sm:justify-between items-center">
+            <div className="flex justify-between items-center gap-2">
+              <div className="rounded-full p-2 border bg-[#DBAC2C]">
+                <Timer color="#fff" weight="fill" size={15}/>
+              </div>
+              Entrega rápida e rastreada
+            </div>
+            <div className="flex justify-between items-center gap-2">
+              <div className="rounded-full p-2 border bg-[#8047F8]">
+                <Coffee color="#fff" weight="fill" size={15}/>
+              </div>
+              O café chega fresquinho até você
+            </div>
           </div>
         </div>
         <div className="hidden md:block">
